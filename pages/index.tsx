bf6b210c1ef7e448550579c229fcde2b00517243
Benjamin-Frost <bfrost.schule@gmail.com>
2022-04-12 15:30:38 +0200

    feat: add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <p>Hello World</p>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default Home;
