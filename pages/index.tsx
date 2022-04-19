9700d9f7142ab7c22f90cc4641d4a1bf24932298
Benjamin Frost <bfrost.schule@gmail.com>
2022-04-19 16:25:14 +0200

    feat: add maintenance page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="pt-16 pb-12 bg-white">
+      <main className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <p className="text-sm font-semibold text-orange-500 uppercase tracking-wide">
+            WorldSkills Germany
+          </p>
+          <h1 className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight">
+            Bundesleistungszentrum der Softwareentwicklung
+          </h1>
+          <p className="mt-2 text-xl text-gray-500">
+            Diese Webseite ist derzeit in Wartung
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
