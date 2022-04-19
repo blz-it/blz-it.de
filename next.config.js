20fd237712861044efb382090cb083ab7c3e5b24
Benjamin Frost <bfrost.schule@gmail.com>
2022-04-19 16:18:30 +0200

    feat: add youtube short link
    Closes #1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/youtube',
+        destination: 'https://www.youtube.com/channel/UCLG45X1YqJeN7fmdd1RT12w',
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
