051d529666dbb3ddddb1767e77f7addd2801bb8d
Jonas Wanke <contact@wanke.dev>
2022-05-19 16:32:47 +0200

    Add remaining social links

--- next.config.js
+++ next.config.js
@@ -4,6 +4,21 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: '/facebook',
+        destination: 'https://www.facebook.com/Skill09.ger',
+        permanent: false,
+      },
+      {
+        source: '/instagram',
+        destination: 'https://www.instagram.com/skill.09/',
+        permanent: false,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/Skill09_ger',
+        permanent: false,
+      },
+      {
         source: '/youtube',
         destination: 'https://www.youtube.com/channel/UCLG45X1YqJeN7fmdd1RT12w',
         permanent: false,
