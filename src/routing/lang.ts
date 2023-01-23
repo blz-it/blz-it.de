8f74f3de04b27302e3a134552cf4c0d15c90e28c
Justin Konratt <justin.konratt@clausohm.de>
2023-01-23 20:41:08 +0100

    hotfix: redicrectLang inside script

--- src/routing/lang.ts
+++ src/routing/lang.ts
@@ -3,11 +3,6 @@ import i18n from "i18next";
 export const getStaticLangPaths = () =>
   i18n.languages.map((lang) => ({ params: { lang } }));
 
-export const redirectLang = (path: string): string | undefined =>
-  i18n.languages.some((lang) => path.startsWith(`/${lang}`))
-    ? undefined
-    : `/${i18n.language}${path}`;
-
 export const updateLang = (path: string) => {
   const lang = i18n.languages.find((lang) => path.startsWith(`/${lang}`));
   lang && i18n.changeLanguage(lang);
