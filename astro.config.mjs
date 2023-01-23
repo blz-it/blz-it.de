ad4fa9d009bb9dba8e598a613fc2f758bd24c004
Benjamin Frost <bfrost.schule@gmail.com>
2023-01-23 14:27:53 +0100

    refactor: react i18n integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
-import astroI18next from "astro-i18next";
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://blz-it.de',
-  integrations: [tailwind(), astroI18next(), react()]
+  site: "https://blz-it.de",
+  integrations: [tailwind(), astroI18next(), react()],
 });
