73b651af71cdde6de826161703803718cfcf1b41
styrix560 <60841004+styrix560@users.noreply.github.com>
2024-07-22 11:54:01 +0200

    Make the dropdown icon customizable

--- src/components/dropdowns/LanguageDropdown.tsx
+++ src/components/dropdowns/LanguageDropdown.tsx
@@ -2,10 +2,13 @@ import { Dropdown } from "./Dropdown";
 import i18n from "i18next";
 import { pathWithoutLanguage, getPathLang } from "../../routing/lang";
 import { useEffect, useState } from "react";
+import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
 type LanguageDropdownProps = {
   astroPath: string;
 };
+
+const upDownChevron = <ChevronUpDownIcon className="h-5 w-5" aria-hidden="true" />;
 
 export const LanguageDropdown = ({ astroPath }: LanguageDropdownProps) => {
   const [pathWithoutLang, setPathWithoutLang] = useState("/");
@@ -21,6 +24,7 @@ export const LanguageDropdown = ({ astroPath }: LanguageDropdownProps) => {
     lang && (
       <Dropdown
         item={lang.toUpperCase()}
+        icon={upDownChevron}
         options={i18n.languages.map((lang) => lang.toUpperCase())}
         onChange={(option) =>
           (location.href = `/${option.toLowerCase()}${pathWithoutLang}`)
