1e92ea263b0f5e09e7282b30086fd0795d143c46
styrix560 <60841004+styrix560@users.noreply.github.com>
2024-07-29 11:21:57 +0200

    Added team data

--- src/utils/async_helper.ts
+++ src/utils/async_helper.ts
@@ -1,6 +1,23 @@
-import history_file from '../../public/data/history.json';
+import history_file from "../../public/data/history.json";
+import team_file from "../../public/data/team.json";
 
-export async function load_history(skill: string, language: string): Promise<Record<string, Record<string, string>>> {
-  const history = (history_file as Record<string, any>)
-  return Promise.resolve(history[language][skill])
+export async function load_history(
+  skill: string,
+  language: string
+): Promise<Record<string, Record<string, string>>> {
+  const history = history_file as Record<string, any>;
+  return Promise.resolve(history[language][skill]);
+}
+
+interface TeamMember {
+  name: string;
+  lastrole: string;
+  img?: string;
+  skill?: string;
+  andmore?: boolean;
+}
+
+export async function load_team(): Promise<TeamMember[]> {
+  const team = team_file as TeamMember[];
+  return Promise.resolve(team);
 }
