980f6188fe884f93740b20f8bcbc7c6b8e21bfd2
styrix560 <60841004+styrix560@users.noreply.github.com>
2024-08-28 12:11:08 +0200

    Use camel case

--- src/utils/async_helper.ts
+++ src/utils/async_helper.ts
@@ -1,6 +1,9 @@
-import history_file from '../../public/data/history.json';
+import history_file from "../../public/data/history.json";
 
-export async function load_history(skill: string, language: string): Promise<Record<string, Record<string, string>>> {
-  const history = (history_file as Record<string, any>)
-  return Promise.resolve(history[language][skill])
+export async function loadHistory(
+  skill: string,
+  language: string
+): Promise<Record<string, Record<string, string>>> {
+  const history = history_file as Record<string, any>;
+  return Promise.resolve(history[language][skill]);
 }
