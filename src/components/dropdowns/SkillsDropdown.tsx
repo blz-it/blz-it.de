2eaa0db9f44412fed70c540421d95cfc43a182be
styrix560 <60841004+styrix560@users.noreply.github.com>
2024-08-28 12:11:31 +0200

    Refactor SkillsDropdown

--- src/components/dropdowns/SkillsDropdown.tsx
+++ src/components/dropdowns/SkillsDropdown.tsx
@@ -4,8 +4,7 @@ import { Dropdown } from "./Dropdown";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { NavigationItem } from "../Header.astro";
 import { buildLink } from "../../routing/buildLink";
-import {t } from "i18next";
-
+import { t } from "i18next";
 
 type SkillsDropdownProps = {
   astroPath: string;
@@ -22,30 +21,22 @@ const skills: NavigationItem[] = [
   },
 ];
 
-const downChevron = <ChevronDownIcon  className="h-5 w-5" aria-hidden="true" />
+const downChevron = <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />;
 
-
-export const SkillsDropdown = ({astroPath} : SkillsDropdownProps) => {
+export const SkillsDropdown = ({ astroPath }: SkillsDropdownProps) => {
   const lang = getPathLang(astroPath);
 
-  useEffect(() => {
-    const path = location.pathname;
-    console.log(location);
-  }, []);
+  if (!lang) return null;
 
   return (
-    lang && (
-      <Dropdown
-        item={t("layout.header.skills")}
-        icon={downChevron}
-        options={skills.map(skill => skill.name)}
-        onChange={(skillName) =>
-          {
-            const skill = skills.find(skill => skill.name === skillName)!;
-            return (location.href = skill.href);
-          }
-        }
-      />
-    )
+    <Dropdown
+      item={t("layout.header.skills")}
+      icon={downChevron}
+      options={skills.map((skill) => skill.name)}
+      onChange={(skillName) => {
+        const skill = skills.find((skill) => skill.name === skillName)!;
+        return (location.href = skill.href);
+      }}
+    />
   );
 };
