67dd05e854b9da14cc27cf0fde8e572e2eb8bffc
Benjamin Frost <bfrost.schule@gmail.com>
2024-10-11 10:35:27 +0200

    feat: add i18n to layout

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,7 @@
-type LocaleKey = "de" | "en";
+import type { AstroGlobal } from "astro";
+import { getRelativeLocaleUrl } from "astro:i18n";
+
+export type LocaleKey = "de" | "en";
 
 interface Locale {
   label: string;
@@ -20,3 +23,27 @@ export const locales: Record<LocaleKey, Locale> = {
 };
 
 export const defaultLocale: Locale = locales.de;
+export const getCurrentLocale = (astro: AstroGlobal) => {
+  return astro.currentLocale
+    ? locales[astro.currentLocale as LocaleKey]
+    : defaultLocale;
+};
+
+export type Translation = { [key in LocaleKey]?: string };
+export const useTranslations = (astro: AstroGlobal) => {
+  return function t(translations: Translation): string {
+    const locale = getCurrentLocale(astro);
+    return translations[locale.lang as LocaleKey] || "";
+  };
+};
+
+interface LocalePath {
+  lang: LocaleKey;
+  path: string;
+}
+export const getLocalePaths = (url: URL): LocalePath[] => {
+  return Object.keys(locales).map((lang) => ({
+    lang: lang as LocaleKey,
+    path: getRelativeLocaleUrl(lang, url.pathname.replace(/^\/[a-zA-Z-]+/, "")),
+  }));
+};
