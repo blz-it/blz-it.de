f9f088b9f8b24b6f6e8208b011c3047400454644
Benjamin Frost <bfrost.schule@gmail.com>
2024-10-14 19:08:28 +0200

    fix: middleware not working

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,25 @@ import { defaultLang, languages } from "./i18n";
 
 export const onRequest = defineMiddleware(async (ctx, next) => {
   if (ctx.url.pathname === "/youtube") {
-    return Response.redirect("https://youtube.de/@GermanITSkills");
+    return Response.redirect("https://youtube.de/@GermanITSkills", 301);
+  }
+
+  if (ctx.url.pathname.startsWith("/_image")) {
+    return next();
   }
 
   const [, lang] = ctx.url.pathname.split("/");
   if (!(lang in languages)) {
     return Response.redirect(
       new URL(`/${defaultLang}${ctx.url.pathname}`, ctx.url),
+      301,
     );
   }
 
   const response = await next();
 
   if (response.status === 404) {
-    return Response.redirect(new URL(`/${lang}/not-found`, ctx.url));
+    return next(new URL(`/${lang}/404`, ctx.url));
   }
 
   return response;
