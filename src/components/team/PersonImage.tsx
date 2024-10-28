79dd74877c754ac77fafc54e6798492607b862bb
styrix560 <60841004+styrix560@users.noreply.github.com>
2024-10-28 18:01:03 +0100

    feat: add team page

--- src/components/team/PersonImage.tsx
+++ src/components/team/PersonImage.tsx
@@ -6,7 +6,7 @@ export const PersonImage = ({ img }: PersonImageProps) => {
   const padding = !img ? "p-5" : "";
   return (
     <img
-      className={`mx-auto h-40 w-40 rounded-full shadow-lg xl:h-56 xl:w-56 object-cover ${padding}`}
+      className={`mx-auto h-40 w-40 rounded-full object-cover shadow-lg xl:h-56 xl:w-56 ${padding}`}
       src={img ?? "/imgs/wsg/wsg_hands.svg"}
       alt=""
     />
