82060d99ef8d92c3d61395d6acd3ad77dcfef038
styrix560 <60841004+styrix560@users.noreply.github.com>
2024-10-29 08:46:20 +0100

    fix: apply suggestions

--- src/components/team/PersonImage.tsx
+++ src/components/team/PersonImage.tsx
@@ -1,12 +1,16 @@
+import clsx from "clsx";
+
 export type PersonImageProps = {
   img?: string;
 };
 
 export const PersonImage = ({ img }: PersonImageProps) => {
-  const padding = !img ? "p-5" : "";
   return (
     <img
-      className={`mx-auto h-40 w-40 rounded-full object-cover shadow-lg xl:h-56 xl:w-56 ${padding}`}
+      className={clsx(
+        `mx-auto h-40 w-40 rounded-full object-cover shadow-lg xl:h-56 xl:w-56`,
+        !img && "p-5",
+      )}
       src={img ?? "/imgs/wsg/wsg_hands.svg"}
       alt=""
     />
